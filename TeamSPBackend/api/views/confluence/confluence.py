6f50cc2ee8dc95fc7f55310da0757c1c5d06e6fd
pinwang <pinwang5776@outlook.com>
2021-04-25 19:00:14 +0800

    get user list by confluence space key, without database

--- TeamSPBackend/api/views/confluence/confluence.py
+++ TeamSPBackend/api/views/confluence/confluence.py
@@ -317,18 +317,25 @@ def get_user_list(request, space_key):
     Method: Get
     Request: page_id
     """
-    #user = request.session.get('user')
-    #username = user['atl_username']
-    #password = user['atl_password']
-    username = "pinwang"
-    password = "W11p30#5776"
+    user = request.session.get('user')
+    username = user['atl_username']
+    password = user['atl_password']
     try:
         confluence = log_into_confluence(username, password)
         response = confluence.get_space_content(space_key, limit=1, expand='restrictions.update.restrictions.user')
         # assume the atl_username is the coordinator's own name, and other than the students, the coordinator
         # is the only one who can read/update this space.
-
-
+        users = response["page"]["results"][0]["restrictions"]["update"]["restrictions"]["user"]["results"]
+        data = []
+        for user in users:
+            if user["username"] != username:
+                user_info = {
+                    "name": user["displayName"],
+                    "id": user["username"],
+                    "email": user["username"] + "@student.unimelb.edu.au",
+                    "picture": "https://confluence.cis.unimelb.edu.au:8443" + user["profilePicture"]["path"]
+                }
+                data.append(user_info)
 
         resp = init_http_response(
             RespCode.success.value.key, RespCode.success.value.msg)
