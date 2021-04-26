417be48199183c236c4e81fb2434cae0a01b4a6b
Jingdan Cui <jcui0002@student.monash.edu>
2021-04-26 20:27:04 +1000

    create meeting_minutes table

--- TeamSPBackend/confluence/models.py
+++ TeamSPBackend/confluence/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class MeetingMinutes(models.Model):
+    meeting_id = models.AutoField(primary_key=True)
+    meeting_title = models.CharField(max_length=256)
+    meeting_link = models.TextField()
+    space_key = models.CharField(max_length=256)
 
+    class Meta:
+        db_table = 'meeting_minutes'
