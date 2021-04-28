1843386149c122d49aae42e06f8dd5c6eebfd935
pete965 <boyangs@student.unimelb.edu.au>
2021-04-28 16:09:20 +0800

    models

--- TeamSPBackend/project/models.py
+++ TeamSPBackend/project/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ProjectCoordinatorRelation(models.Model):
-    coordinator_id = models.IntegerField(max_length=256, null=False)
+    coordinator_id = models.IntegerField(null=False)
     space_key = models.CharField(max_length=256, null=False)
     git_url = models.CharField(max_length=256, null=False)
     jira_project = models.CharField(max_length=256, null=False)
