25753d53e7b5931d86c79b1e64a44e089f82f94b
pete965 <boyangs@student.unimelb.edu.au>
2021-04-28 11:25:29 +0800

    models

--- TeamSPBackend/project/models.py
+++ TeamSPBackend/project/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class ProjectCoordinatorRelation(models.Model):
     coordinator_id = models.CharField(max_length=256, null=False)
     space_key = models.CharField(max_length=256, null=False)
-    git_url = models.CharField(max_length=256, null=True)
-    jira_project = models.CharField(max_length=256, null=True)
+    git_url = models.CharField(max_length=256, null=False)
+    jira_project = models.CharField(max_length=256, null=False)
 
     class Meta:
         db_table = 'project_coordinator_relation'
