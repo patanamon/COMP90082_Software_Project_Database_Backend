8e5fa26e1b1adcedf8f59f6adc3fec544bd5f7bc
pete965 <boyangs@student.unimelb.edu.au>
2021-04-28 11:21:09 +0800

    models

--- TeamSPBackend/coordinator/models.py
+++ TeamSPBackend/coordinator/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Coordinator(models.Model):
     # github username for accessing private repositories
-    git_username = models.CharField(max_length=256, null=False)
-    git_password = models.CharField(max_length=256, null=False)
+    git_username = models.CharField(max_length=256, null=True)
+    git_password = models.CharField(max_length=256, null=True)
     # atlassian username and password
-    atl_username = models.CharField(max_length=256, null=False)
-    atl_password = models.CharField(max_length=256, null=False)
+    atl_username = models.CharField(max_length=256, null=True)
+    atl_password = models.CharField(max_length=256, null=True)
     class Meta:
         db_table = 'coordinator'
--- TeamSPBackend/project/models.py
+++ TeamSPBackend/project/models.py
@@ -4,9 +4,8 @@ from django.db import models
 class ProjectCoordinatorRelation(models.Model):
     coordinator_id = models.CharField(max_length=256, null=False)
     space_key = models.CharField(max_length=256, null=False)
-    git_url = models.CharField(max_length=256, null=False)
-    jira_project = models.CharField(max_length=256, null=False)
-
+    git_url = models.CharField(max_length=256, null=True)
+    jira_project = models.CharField(max_length=256, null=True)
 
     class Meta:
         db_table = 'project_coordinator_relation'
