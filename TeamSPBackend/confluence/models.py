da0e3cf590a443decdef792e0b33b240fce609ea
pinwang5776 <pinwang5776@outlook.com>
2021-04-28 11:32:49 +0800

    remove null=False, because it is by default.

--- TeamSPBackend/confluence/models.py
+++ TeamSPBackend/confluence/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 
 class PageHistory(models.Model):
-    page_count = models.IntegerField(null=False)
-    date = models.IntegerField(null=False)
-    space_key = models.CharField(max_length=256, null=False)
+    page_count = models.IntegerField()
+    date = models.IntegerField()
+    space_key = models.CharField(max_length=256)
 
     class Meta:
         db_table = 'page_history'
