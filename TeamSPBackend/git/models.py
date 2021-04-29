635640d3c202391cf9397cb231014398aeaf1a4b
ChongjingZhang <121943974@qq.com>
2021-04-29 19:41:09 +0800

    zcj modify

--- TeamSPBackend/git/models.py
+++ TeamSPBackend/git/models.py
@@ -13,3 +13,13 @@ class StudentCommitCounts(models.Model):
 
     class Meta:
         db_table = 'student_commit_counts'
+
+
+class GitCommitCounts(models.Model):
+    # student_name = models.CharField(max_length=256, null=False)
+    space_key = models.CharField(max_length=256, null=False)
+    commit_counts = models.CharField(max_length=256, null=False)
+    query_date = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'git_commit_counts'
