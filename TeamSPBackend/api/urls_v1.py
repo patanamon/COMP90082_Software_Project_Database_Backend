ec5e3c9976e14a34e95b309594ff13313ba2c84e
pinwang <pinwang5776@outlook.com>
2021-05-12 00:23:22 +0800

    delete imported project

--- TeamSPBackend/api/urls_v1.py
+++ TeamSPBackend/api/urls_v1.py
@@ -10,7 +10,7 @@ from .views.account import account_router, login, logout, update_account, delete
 from .views.subject import subject_router, update_subject, delete_subject
 from .views.team import team_router, get_team_members, team_member_configure, team_configure
 from .views.slack import get_team_data, get_all_member_data, get_member_data
-from TeamSPBackend.api.views.project.project import import_project_in_batch, login_sso
+from TeamSPBackend.api.views.project.project import import_project_in_batch
 from .views.git import get_git_commits, get_git_pr, get_git_individual_commits
 
 
