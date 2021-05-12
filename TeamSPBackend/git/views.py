b9698acd0886fae45af5e60936d3a0c4a04e2062
ChongjingZhang <121943974@qq.com>
2021-05-12 22:39:34 +0800

    optimize authentication exception problems, update git_commit code and add sort function

--- TeamSPBackend/git/views.py
+++ TeamSPBackend/git/views.py
@@ -136,5 +136,5 @@ def auto_update_commits():
 
 
 utils.start_schedule(auto_update_commits, 60 * 60 * 24)
-# utils.start_schedule(update_individual_commits, 60 * 60 * 24)
+utils.start_schedule(update_individual_commits, 60 * 60 * 24)
 
