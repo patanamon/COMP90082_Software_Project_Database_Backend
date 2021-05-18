2bf765098518190848130f355cd262115b1327e6
xiefx <70609741+xiefx@users.noreply.github.com>
2021-05-19 03:08:26 +1000

    change table name, store jira and git url in relation table, fix bugs for jira api, enable scheduled tasks

--- TeamSPBackend/api/views/jira/__init__.py
+++
@@ -1,3 +0,0 @@
-# enable the code below to make jira.auto_get_ticket_count_team_timestamped() run on server startup, the process of fetching all data could be slow
-
-# default_app_config = 'TeamSPBackend.api.views.jira.apps.AutoConfig'
