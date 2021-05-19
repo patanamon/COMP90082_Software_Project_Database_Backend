e447b6914f1c6d93f8059f3b274ba3bb8c394435
pinwang5776 <pinwang5776@outlook.com>
2021-05-19 16:10:38 +0800

    add time log for import project

--- TeamSPBackend/api/views/project/project.py
+++ TeamSPBackend/api/views/project/project.py
@@ -1,8 +1,12 @@
+import sys
+
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 import json
 import operator
 import requests
+import time
+import logging
 
 from TeamSPBackend.common.choices import RespCode
 from TeamSPBackend.coordinator.models import Coordinator
@@ -16,6 +20,7 @@ from TeamSPBackend.confluence.views import insert_space_user_list, insert_space_
 # @check_body
 def import_project(request, *args, **kwargs):
     # Method: POST
+    start = time.time()
     try:
         json_body = json.loads(request.body)
         space_key = json_body.get("space_key")
@@ -30,8 +35,13 @@ def import_project(request, *args, **kwargs):
             Timer(0, insert_space_meeting, args=(space_key,)).start()
         resp = init_http_response(
             RespCode.success.value.key, RespCode.success.value.msg)
+        logger = logging.getLogger('django')
+        logger.info("import project " + space_key + " takes " + str(time.time() - start) + "s")
+
         return HttpResponse(json.dumps(resp), content_type="application/json")
-    except:
+    except Exception as e:
+        tb = sys.exc_info()[2]
+        logger.error(e.with_traceback(tb))
         resp = {'code': -1, 'msg': 'error'}
         return HttpResponse(json.dumps(resp), content_type="application/json")
 
