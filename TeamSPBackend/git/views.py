a0c6c9fa9d238696842ba5ae0988413d77e1f5c1
Jerry-Shan <24691155+Jerry-Shan@users.noreply.github.com>
2021-05-23 19:41:53 +0800

    Bugfix-GitMetrics

--- TeamSPBackend/git/views.py
+++ TeamSPBackend/git/views.py
@@ -218,4 +218,4 @@ def auto_update_metrics():
 
 utils.start_schedule(auto_update_commits, 60 * 60 * 24, None)
 utils.start_schedule(update_individual_commits, 60 * 60 * 24)
-utils.start_schedule(auto_update_metrics, 60 * 60 * 24)
+# utils.start_schedule(auto_update_metrics, 60 * 60 * 24)
